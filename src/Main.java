ca622106b716eac2e306673ef7df32e385289db9
vx007 <vasilyev.x@mail.ru>
2022-11-15 00:18:39 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) { }
+    public static HashMap<String, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        addToMap("str1", 2);
+        System.out.println(map);
+        addToMap("str2", 1);
+        System.out.println(map);
+        addToMap("str1", 2);
+        System.out.println(map);
+        addToMap("str1", 5);
+        System.out.println(map);
+    }
+
+    public static void addToMap(String key, Integer value) {
+        if (!map.containsKey(key)) {
+            map.put(key, value);
+        } else if (map.get(key).equals(value)) {
+            System.out.println("Такая пара КЛЮЧ-ЗНАЧЕНИЕ существует!");
+        } else {
+            map.replace(key, value);
+        }
+    }
 }
